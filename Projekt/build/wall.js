58fe82874406ed211be1cf710bc378c75b28b075
mats.klein@stud.hs-flensburg.de <MAcl9700>
2020-12-17 00:12:53 +0100

    implemented structure for search requests

--- Projekt/build/wall.js
+++ Projekt/build/wall.js
@@ -7,14 +7,43 @@ window.onload = function () {
             return response.json();
         }).then(function (data) {
             insertTweets(data);
+            initButtons();
         });
     }
     function initButtons() {
         var submit = document.getElementById("submitSearch");
-        submit.addEventListener("click", search());
+        submit.addEventListener("click", search);
     }
     function search() {
         var search = document.getElementById("tweet-search");
+        var rule = {
+            add: [{
+                    value: search.value,
+                    tag: search.value
+                }]
+        };
+        var addRule = new XMLHttpRequest();
+        addRule.open('POST', 'https://api.twitter.com/2/tweets/search/stream/rules');
+        addRule.send(JSON.stringify(rule));
+        addRule.onreadystatechange = function () {
+            if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+                console.log("Adding Rule: Success!");
+            }
+            else {
+                console.log("Adding Rule: Error");
+            }
+        };
+        var getStream = new XMLHttpRequest();
+        getStream.open('GET', 'https://api.twitter.com/2/tweets/search/stream?tweet.fields=created_at&expansions=author_id&user.fields=created_at');
+        getStream.send();
+        addRule.onreadystatechange = function () {
+            if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+                console.log(getStream.responseText);
+            }
+            else {
+                console.log("Get Stream: Error");
+            }
+        };
     }
     function insertTweets(data) {
         data.dummy.forEach(function (tweet) {
