80234375f92c942c67d7309cbf501daee8152f74
mats.klein@stud.hs-flensburg.de <MAcl9700>
2021-01-12 23:05:31 +0100

    vee-validate install

---
+++ Projekt/client/src/components/TheLogin.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-form>
+  </v-form>
+</template>
+
+<script>
+export default {
+  name: 'TheLogin'
+}
+</script>
+
+<style scoped>
+
+</style>
--- Projekt/client/src/main.ts
+++ Projekt/client/src/main.ts
@@ -5,12 +5,21 @@ import vuetify from './plugins/vuetify'
 import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import { ValidationProvider } from 'vee-validate'
+
 const VueMasonryPlugin = require('vue-masonry').VueMasonryPlugin
 
 Vue.config.productionTip = false
+
+// Axios
 Vue.use(VueAxios, axios)
-Vue.use(VueMasonryPlugin)
 Vue.axios.defaults.baseURL = 'http://localhost:9000/'
+
+// Masonry-Layout
+Vue.use(VueMasonryPlugin)
+
+// Form-Validation (Vee-Validate)
+Vue.component('ValidationProvider', ValidationProvider)
 
 new Vue({
   router,
